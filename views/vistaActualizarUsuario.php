914a48d8c3074cb4873db89a11ac5769d76db8e2
josephinocente <joseph.inocente@unas.edu.pe>
2024-11-27 22:09:51 -0500

    cuarto hito actualizado

--- views/vistaActualizarUsuario.php
+++ views/vistaActualizarUsuario.php
@@ -1,44 +1,46 @@
 <?php
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/models/modeloUsuario.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/views/vistaActualizarUsuario.php';
-
-function mostrarFormularioBusqueda($mensaje = '') {
-    if (empty($mensaje)) {
+function mostrarFormularioBusqueda($mensaje = '')
+{
+    if (!empty($mensaje)){
         echo $mensaje;
         echo "<br>";
     }
-    ?>
-<form action="/controllers/controladorActualizarUsuario.php" method="POST">
-    <label for="">Que usuario deseas modificar</label>
-    <input type="text" name="buscarusuario" id="buscarusuario">
-    <br>
-    <button type="submit">Buscar usuario</button>
-</form>
-    <?php
+?>
+    <form action="/controllers/controladorActualizarUsuario.php" method="POST">
+        <label for="">QUE USUARIO DESEAS MODIFICAR</label>
+        <br>
+        <input type="text" name="datusuario" id="datusuario" autocomplete="off">
+        <br>
+        <button type="submit">bucar Usuario</button>
+    </form>
+
+<?php
 }
 
-function mostrarFormularioEdicion($usuario, $mensaje= '') {
-    if (empty($mensaje)) {
-        echo $mensaje;
-        echo "<br>";
-    }
-    ?>
-<form action="/controllers/controladorActualizarUsuario.php" method="POST">
-            <input type="hidden" id="custId" name="custId" value="<?php echo $usuario['id'] ?>">
+function mostrarFormularioEdicion($usuario,$mensaje = '')
+{
+?>
+    <form action="/controllers/controladorActualizarUsuario.php" method="POST">
 
-            <label for="datusuario">Usuario</label>
-            <input type="text" name="datusuario" id="datusuario" value="<?php echo $usuario['username'] ?>">
-            <br>
-            <label for="datpasword">password</label>
-            <input type="password" name="datpassword" id="datpassword" value="<?php echo $usuario['password'] ?>">
-            <br>
-            <label for="datperfil">usuario</label>
-            <input type="text" name="datperfil" id="datperfil" value="<?php echo $usuario['perfil'] ?>">
-            <br>
-            <button type="submit">Modificar usuario</button>
-        </form>
-    <?php
+        <input type="hidden" name="custId" id="custId" value=" <?php echo $usuario['id'] ?>">
+        <br>
+        <label for="datusuario"> usuario</label>
+        <input type="text" name="datusuario" id="datusuario" autocomplete="off" value="<?php echo $usuario['username'] ?>">
+        <br>
+        <label for="datpassword"> password </label>
+        <input type="password" name="datpassword" id="datpassword" autocomplete="off" value="<?php echo$usuario['password'] ?>">
+        <br>
+        <label for="datperfil"> perfil </label>
+        <input type="text" name="datperfil" id="datperfil" autocomplete="off" value="<?php echo $usuario['perfil'] ?>">
+        <br>
+
+        <button type="submit">MODIFICAR USUARIO</button>
+
+
+
+    </form>
+<?php
 }
 
 ?>
